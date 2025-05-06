3f0daaa4e0fa66fb7c8966ba1490a8ef52ebafa5
xProBomb <Justbomb03@gmail.com>
2025-05-05 18:39:20 -0700

    implemented routing for /api/calcs/<value>

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
+@app.route("/api/calcs/<value>", methods=["GET"])
+def calculate(value):
+    try:
+        num = int(value)
+        if num <= 0:
+            raise ValueError
+    except ValueError:
+        return {}, 404
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+    return {
+        "dec": num - 1,
+        "hex": hex(num)
+    }
+
+if __name__ == "__main__":
+    app.run(port=8080)
